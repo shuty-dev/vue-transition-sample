f6f3539bbd40cca79b67491d64199690150f340d
shutoinoue <developer1019wilshere@gmail.com>
2025-02-11 16:01:56 +0900

    add transition with name

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 const show = ref(true)
+const show2 = ref(true)
 </script>
 
 <template>
-  <button @click="show = !show">Toggle</button>
-  <Transition>
-    <p v-if="show">hello</p>
-  </Transition>
+  <div>
+    <button @click="show = !show">Toggle</button>
+    <Transition>
+      <p v-if="show">hello</p>
+    </Transition>
+  </div>
+  <div>
+    <button @click="show2 = !show2">Toggle2</button>
+    <Transition name="hogehoge">
+      <p v-if="show2">hello</p>
+    </Transition>
+  </div>
 </template>
 
 <style scoped>
@@ -18,6 +27,20 @@ const show = ref(true)
 
 .v-enter-from,
 .v-leave-to {
+  opacity: 0;
+}
+
+.hogehoge-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.hogehoge-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.hogehoge-enter-from,
+.hogehoge-leave-to {
+  transform: translateX(20px);
   opacity: 0;
 }
 </style>
